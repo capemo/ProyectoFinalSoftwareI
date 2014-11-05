c8d18a291a94c3ece66434c61951f2868ace994d
Camilo <Camilo@Camilo-PC>
2014-11-05 17:19:07 -0500

    Lectura de archivo Programa.txt

--- Proyecto Final (Ing. Software)/src/persistence/StudentProgramLoader.java
+++ Proyecto Final (Ing. Software)/src/persistence/StudentProgramLoader.java
@@ -2,11 +2,15 @@ package persistence;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.swing.*;
 
+import model.Estado;
+import model.EstadoMateria;
 import model.Materia;
+import model.Periodo;
 import model.Programa;
 
 public class StudentProgramLoader {
@@ -22,45 +26,54 @@ public class StudentProgramLoader {
 			UIManager
 					.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
 		} catch (Exception e) {
-			
+
 		}
 		JFileChooser chooser = new JFileChooser();
 		File file = new File("Programas académicos/");
-        chooser.setCurrentDirectory(file);
-		
+		chooser.setCurrentDirectory(file);
+
 		int filename = chooser.showOpenDialog(null);
-		
+
 		if (filename == JFileChooser.APPROVE_OPTION) {
 			File f = chooser.getSelectedFile();
 			try {
 				FileReader inputFile = new FileReader(f);
 
 				BufferedReader bufferReader = new BufferedReader(inputFile);
-				List<Materia> materias=new ArrayList<Materia>();
+				List<Materia> materias = new ArrayList<Materia>();
 				String line;
-				int cont=1;
-				String nombrePrograma="";
-				String materia;
-				
-				while ((line = bufferReader.readLine()) != null) {
-					if (line.contains("-- PRE-REQUICITOS --")) {
-						continue pre_requicitos;
+				int cont = 1;
+				String nombrePrograma = "";
+				String[] materia;
+				String[] preRequicitos;
+
+				while (!(line = bufferReader.readLine())
+						.contains("-- PRE-REQUICITOS --")) {
+					if (cont == 1) {
+						nombrePrograma = line;
+						cont = 0;
+					} else {
+						materia=line.split(" ~ ");
+						materias.add(new Materia(materia[0], materia[1], new ArrayList<Materia>(), Integer.parseInt(materia[2]), new EstadoMateria(Estado.NO_VISTA, (Calendar.getInstance().get(Calendar.YEAR)), Periodo.I)));
 					}
-					if (cont==1) {
-						nombrePrograma=line;
-						cont=0;
-					}
-					materias.add(new Materia(idMateria, nombreMateria, new ArrayList<Materia>(), creditos, estado)
 				}
-				pre_requicitos: {
-					while ((line = bufferReader.readLine()) != null) {
-						for (int i = 0; i < array.length; i++) {
-							
+
+				while ((line = bufferReader.readLine()) != null) {
+					preRequicitos=line.split(" ¬ ");
+					for (int i = 0; i < materias.size(); i++) {
+						if (materias.get(i).getIdMateria().equals(preRequicitos[0])) {
+							for (int j = 1; j < preRequicitos.length; j++) {
+								for (int k = 0; k < materias.size(); k++) {
+									if (materias.get(k).getIdMateria().equals(preRequicitos[j])) {
+										materias.get(i).getPreRequicitos().add(materias.get(k));
+									}
+								}
+							}
 						}
 					}
 				}
-				
-				Programa programa=new Programa(nombrePrograma, materias, null);
+
+				Programa programa = new Programa(nombrePrograma, materias, "");
 
 				bufferReader.close();
 			} catch (Exception e) {
