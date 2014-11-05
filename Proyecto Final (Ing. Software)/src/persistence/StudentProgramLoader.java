07b2bfb8572e52b879c1b0a9f876aff13de1415f
Camilo <Camilo@Camilo-PC>
2014-11-05 15:42:02 -0500

    Init

--- Proyecto Final (Ing. Software)/src/persistence/StudentProgramLoader.java
+++ Proyecto Final (Ing. Software)/src/persistence/StudentProgramLoader.java
@@ -1,8 +1,13 @@
 package persistence;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.*;
+
+import model.Materia;
+import model.Programa;
 
 public class StudentProgramLoader {
 	public StudentProgramLoader() {
@@ -20,21 +25,42 @@ public class StudentProgramLoader {
 			
 		}
 		JFileChooser chooser = new JFileChooser();
-
+		File file = new File("Programas académicos/");
+        chooser.setCurrentDirectory(file);
+		
 		int filename = chooser.showOpenDialog(null);
-
+		
 		if (filename == JFileChooser.APPROVE_OPTION) {
 			File f = chooser.getSelectedFile();
 			try {
 				FileReader inputFile = new FileReader(f);
 
 				BufferedReader bufferReader = new BufferedReader(inputFile);
-
+				List<Materia> materias=new ArrayList<Materia>();
 				String line;
-
+				int cont=1;
+				String nombrePrograma="";
+				String materia;
+				
 				while ((line = bufferReader.readLine()) != null) {
-					System.out.println(line);
+					if (line.contains("-- PRE-REQUICITOS --")) {
+						continue pre_requicitos;
+					}
+					if (cont==1) {
+						nombrePrograma=line;
+						cont=0;
+					}
+					materias.add(new Materia(idMateria, nombreMateria, new ArrayList<Materia>(), creditos, estado)
 				}
+				pre_requicitos: {
+					while ((line = bufferReader.readLine()) != null) {
+						for (int i = 0; i < array.length; i++) {
+							
+						}
+					}
+				}
+				
+				Programa programa=new Programa(nombrePrograma, materias, null);
 
 				bufferReader.close();
 			} catch (Exception e) {
